5e3431886c21c985206c93d1b96d990868780447
zero2603 <20153713@student.hust.edu.vn>
2019-08-27 22:21:48 +0700

    express

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var crawler = require('./crawler');
+var express = require('express');
+var fs = require('fs');
 
-console.log(crawler.crawlMovies(2));
+var app = express();
+app.get('/movies', (req, res) => {
+    var filename = `movie_${req.query.date}.json`;
+
+    var data = fs.readFileSync(`./data/movies/${filename}`);
+    return {data};
+});
